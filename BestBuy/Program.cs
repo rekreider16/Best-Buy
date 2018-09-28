e6993e23428500e160490e0ff685d281299c45c6
Evan <rekreider@una.edu>
2018-09-28 13:41:06 -0500

    Add project files.

---
+++ BestBuy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
